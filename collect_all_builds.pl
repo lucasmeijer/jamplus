abe6b4d1eafa3d6dcb5887a6e8b18c4f88ea918c
Lucas Meijer <lucas@lucasmeijer.com>
2010-10-05 15:37:06 +0200

    add a buildscript to fix the executable bits after teamcity resolved the dependencies (sigh)

---
+++ collect_all_builds.pl
@@ -0,0 +1,11 @@
+open(MYFILE,">built_by_teamcity.txt");
+print MYFILE "These builds were created by teamcity from vcs number: $ENV{BUILD_VCS_NUMBER}\n";
+print MYFILE "TC projectname was: $ENV{TEAMCITY_PROJECT_NAME}\n";
+print MYFILE "TC buildconfigname was: $ENV{TEAMCITY_BUILDCONF_NAME}\n";
+close(MYFILE);
+
+system("chmod +x bin/macosxx86/*");
+system("chmod +x bin/macosxx64/*");
+system("chmod +x bin/linuxx86/*");
+
+system("zip -r builds.zip *") eq 0 or die("failed zipping up builds");
